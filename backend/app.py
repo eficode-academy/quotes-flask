fa8fba30d533f7e05a31f59282cff3400a1aa88e
Sofus Albertsen <sofus.albertsen@eficode.com>
2022-12-02 13:08:19 +0100

    ADD backend root response "Hello from the backend!"

--- backend/app.py
+++ backend/app.py
@@ -109,6 +109,9 @@ def add_quote():
         return "Quote received", 200
     return "Could not parse quote", 500
 
+@app.route("/")
+def index():
+    return "Hello from the backend!"
 
 @app.route("/quotes")
 def quotes():
