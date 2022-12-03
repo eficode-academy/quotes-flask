8b7018dfc65fe03014548bc5e8a6bcb61216fded
zanderhavgaard <zander@havgaard.dk>
2022-12-04 00:22:45 +0100

    clean up frontend app.py, fix lots of pylint errors/warnings

--- frontend/app.py
+++ frontend/app.py
@@ -5,22 +5,27 @@ and provides endpoints for getting/pushing quotes.
 import os
 import random
 import socket
-import requests
 import logging
+import requests
+from kubernetes import client as kubernetes_client
+from kubernetes import config as kubernetes_config
 from flask import Flask, render_template, jsonify, request
+from flask.wrappers import Response
+from flask.logging import create_logger
 from flask_healthz import healthz
 from quotes import default_quotes
 
 # configure logging
-logging.basicConfig(level=logging.DEBUG, format=f"%(asctime)s %(levelname)s: %(message)s")
+logging.basicConfig(level=logging.INFO)
 
 # create the flask app
-app = Flask(__name__)
+APP = Flask(__name__)
+log = create_logger(APP)
 
 # add flask-healthz config to flask config
-app.config["HEALTHZ"] = {"live": "healthz.liveness", "ready": "healthz.readiness"}
+APP.config["HEALTHZ"] = {"live": "healthz.liveness", "ready": "healthz.readiness"}
 # create the healthz endpoints
-app.register_blueprint(healthz, url_prefix="/healthz")
+APP.register_blueprint(healthz, url_prefix="/healthz")
 
 
 # Read environment variables
@@ -30,16 +35,20 @@ BACKEND_PORT = os.environ.get("backend_port", False)
 BACKEND_ENDPOINT = bool(BACKEND_HOST and BACKEND_PORT)
 # build the url for the backend
 BACKEND_URL = f"http://{BACKEND_HOST}:{BACKEND_PORT}"
+# whether the container is running in kubernetes, assumes that it is
+NOT_RUNNING_IN_KUBERNETES = bool(os.environ.get("not_running_in_kubernetes", False))
 
 
 def check_backend_endpoint_env_var() -> bool:
     """Checks if the user has set the backend host environment variable"""
     if BACKEND_ENDPOINT:
-        app.logger.info(
-            f"Found 'backend_host' environment variable, will attempt to connect to the backend on: {BACKEND_URL}"
+        log.info(
+            str.format(
+                "Found 'backend_host' environment variable, will attempt to connect to the backend on: %s", BACKEND_URL
+            )
         )
         return True
-    app.logger.warning("'backend_host' environment variable not set, set this to connect to the backend.")
+    log.warning("'backend_host' environment variable not set, set this to connect to the backend.")
     return False
 
 
@@ -50,7 +59,7 @@ def check_if_database_is_available() -> bool:
         # try querying the backend
         try:
             backend_health_endpoint = f"{BACKEND_URL}/check-db-connection"
-            response = requests.get(backend_health_endpoint)
+            response = requests.get(backend_health_endpoint, timeout=1)
             if response.status_code == 200:
                 body = response.json()
                 if "db-connected" in body:
@@ -73,7 +82,7 @@ def check_if_backend_is_available() -> bool:
         # try querying the backend
         try:
             backend_health_endpoint = f"{BACKEND_URL}/healthz/ready"
-            response = requests.get(backend_health_endpoint)
+            response = requests.get(backend_health_endpoint, timeout=1)
             return response.status_code == 200
         except requests.ConnectionError:
             return False
@@ -83,23 +92,23 @@ def check_if_backend_is_available() -> bool:
 
 def get_random_quote_from_backend() -> str:
     """get a single quote from the backend"""
-    response = requests.get(f"{BACKEND_URL}/quote")
+    response = requests.get(f"{BACKEND_URL}/quote", timeout=1)
     if response.status_code == 200:
         return response.text
-    app.logger.error("did not get a response 200 from backend")
+    log.error("did not get a response 200 from backend")
     return ""
 
 
 def get_all_quotes_from_backend() -> list[str]:
     """get list of all quotes form the backend"""
-    response = requests.get(f"{BACKEND_URL}/quotes")
+    response = requests.get(f"{BACKEND_URL}/quotes", timeout=1)
     if response.status_code == 200:
         return response.json()
-    app.logger.error("did not get a response 200 from backend")
+    log.error("did not get a response 200 from backend")
     return []
 
 
-@app.route("/")
+@APP.route("/")
 def index():
     """Main endpoint, serves the frontend"""
     # check if the backend and database are available and communicate this to user
@@ -124,7 +133,7 @@ def index():
     )
 
 
-@app.route("/random-quote")
+@APP.route("/random-quote")
 def random_quote():
     """return a random quote"""
     if check_if_backend_is_available():
@@ -133,7 +142,7 @@ def random_quote():
     return random.choice(default_quotes)
 
 
-@app.route("/quotes")
+@APP.route("/quotes")
 def quotes():
     """Get all available quotes as JSON"""
     if check_if_backend_is_available():
@@ -141,57 +150,55 @@ def quotes():
     return jsonify(default_quotes)
 
 
-@app.route("/add-quote", methods=["POST"])
+@APP.route("/add-quote", methods=["POST"])
 def add_quote():
     """receive quote and pass it on to the backend"""
-    app.logger.info("attempting to add new quote to backend ...")
+    log.info("attempting to add new quote to backend ...")
     if request.method == "POST":
         request_json = request.get_json()
-        app.logger.info(f"recieved JSON: {request_json}")
+        log.info(f"recieved JSON: {request_json}")
 
         if "quote" in request_json:
             url = f"{BACKEND_URL}/add-quote"
             try:
-                res = requests.post(url, json=request_json)
+                res = requests.post(url, json=request_json, timeout=1)
                 if res.status_code == 200:
-                    app.logger.info("new quote successfully posted to backend.")
+                    log.info("new quote successfully posted to backend.")
                     return "Quote received", 200
-                app.logger.error("could not successfully post new quote to backend.")
+                log.error("could not successfully post new quote to backend.")
                 return "error inserting quote", 500
             except (requests.ConnectionError, KeyError) as err:
-                app.logger.error("encountered error when trying to pass quote to backend:")
-                app.logger.error(err)
+                log.error("encountered error when trying to pass quote to backend:")
+                log.error(err)
                 return "error inserting quote", 500
         else:
-            app.logger.error("could not find 'quote' in request")
+            log.error("could not find 'quote' in request")
             return "No 'quote' key in JSON", 500
     return "Could not parse quote", 500
 
 
-def get_hostnames() -> (str, str):
+def get_hostnames() -> tuple[str, str]:
     """returns tuple of (frontend_hostname, backend_hostname)"""
-    app.logger.info("Attempting to get backend hostname ...")
+    log.info("Attempting to get backend hostname ...")
     frontend_hostname = socket.gethostname()
     if check_if_backend_is_available():
         try:
-            response = requests.get(f"{BACKEND_URL}/hostname")
+            response = requests.get(f"{BACKEND_URL}/hostname", timeout=1)
             if response.status_code == 200:
                 resp_json = response.json()
-                app.logger.debug(resp_json)
+                log.debug(resp_json)
                 if "backend" in resp_json:
-                    #  hostnames["backend"] = resp_json["backend"]
                     backend_hostname = resp_json["backend"]
                     return (frontend_hostname, backend_hostname)
         except (requests.ConnectionError, KeyError) as err:
-            app.logger.error("Encountered an error trying to get hostname from backend: ")
-            app.logger.error(err)
-            hostnames["backend"] = "backend_not_connected"
+            log.error("Encountered an error trying to get hostname from backend: ")
+            log.error(err)
             return (frontend_hostname, None)
-    app.logger.error("did not get a response 200 from backend")
+    log.error("did not get a response 200 from backend")
     return (frontend_hostname, None)
 
 
-@app.route("/hostname")
+@APP.route("/hostname")
 def hostname():
     """return the hostname of the given container"""
     frontend_hostname, backend_hostname = get_hostnames()
