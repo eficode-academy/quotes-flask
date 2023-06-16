d236b6a8d2f7b45a41fcfcc2a09adad20382321f
Sofus Albertsen <sofus.albertsen@eficode.com>
2023-06-16 15:20:53 +0200

    Initial helm creation

---
+++ ci/make_helm_release.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+REGISTRY="ghcr.io/eficode-academy"
+FRONTEND_REPOSITORY="quotes-flask-frontend"
+BACKEND_REPOSITORY="quotes-flask-backend"
+HELM_REPOSITORY="quotes-flask-helm"
+VERSIONS="
+1.0.0
+2.0.0
+3.0.0
+4.0.0
+5.0.0
+"
+
+echo "Creating Helm releases ..."
+cd helm
+
+for VERSION in $VERSIONS; do
+    echo "Creating Helm release: $VERSION"
+    FRONTEND_IMAGE="$REGISTRY/$FRONTEND_REPOSITORY:$VERSION"
+    BACKEND_IMAGE="$REGISTRY/$BACKEND_REPOSITORY:$VERSION"
+    echo "generating helm release for $VERSION"
+	VERSION=$VERSION yq -i '.version = env(VERSION)' quotes-flask/Chart.yaml
+    VERSION=$VERSION yq -i '.appVersion = env(VERSION)' quotes-flask/Chart.yaml
+    FRONTEND_IMAGE=$FRONTEND_IMAGE yq -i '.spec.template.spec.containers[0].image = env(FRONTEND_IMAGE)' quotes-flask/templates/frontend-deployment.yaml
+    BACKEND_IMAGE=$BACKEND_IMAGE yq -i '.spec.template.spec.containers[0].image = env(BACKEND_IMAGE)' quotes-flask/templates/backend-deployment.yaml
+    echo "helm push quotes-flask $VERSION"
+
+done
